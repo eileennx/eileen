9aaae4b3cb770892fee48597f15d72997d2c5722
Eileen Xie <eileenx@berkeley.edu>
2026-02-09 13:28:43 -0800

    done with hw2

---
+++ homework2/homework2.py
@@ -0,0 +1,67 @@
+# File: homework2.py
+
+# Your file path should look like:
+# python_decal_fa25/yourname/homework2/homework2.py
+
+# Questions (Answer these in the homework2.py file as comments):
+
+# 1) What’s the difference between Git, GitHub, and Git Bash?
+# Git is the local tool (engine), GitHub is the website (cloud), and Git Bash is the terminal window (interface).
+
+# 2) What’s the difference between the terminal and the command line?
+# The terminal is the window that wraps around the comman line (like the box you type in) and the command line is the actual interface where you type text commands to the computer.
+
+# 3) How does Windows PowerShell differ from Git Bash?
+# Windows PowerShell uses Windows commands which Git Bash uses Linux/Unix commands.
+
+# 4) What’s the difference between Anaconda, conda, and Python?
+# Python is the language, Conda is the package manager, and Anaconda is the full software distribution.
+
+# 5) What is VS Code? 
+# VS code is a text editor used to write and debug code.
+
+# 6) What is a Jupyter Notebook? How is it different from Jupyter Lab?
+# Jupyter Notebook is a single file format while Lab is a workspace that handles multiple notebooks and files at once.
+
+# 7) What does ~/ mean?
+# ~/ is a shortcut for the computer's home directory.
+
+# 8) What’s the difference between an absolute path and a relative path?
+# The absolute path starts from the computer's root while the relative path starts from the current folder you're in.
+
+# 9) Imagine you're in your "yourname" repo. Write the absolute and relative paths to "course_assignments/homework2".
+# Absolute: /c/Users/eilee/python_decal_sp26/course_assignments/homework2/
+# Relative: ../course_assignments/homework2/
+
+# 10) What command lets you move from "course_assignments/homework2/" to "course_assignments/"?
+# cd ..
+
+# 11) What would rm ./ do in your current directory? (Don’t try it!)
+# Deletes everything in your current directory.
+
+# 12) What do the following commands do?
+# git add: stages changes
+# git commit: saves changes locally
+# git push: uploads changes to GitHub
+
+# 13) What's the difference between "git add ." and "git add <file>"?
+# add . stages all changes while add <file> stages only that file
+
+# 14) What do "git status" and "git log -1" do?
+# git status shows current changes and log - 1 shows the single most recent saved commit
+
+# 15) What’s the difference between cloning a repository and pulling from it?
+# Cloning downloads a repo for the first time and pulling updates a repo that you already have.
+
+# 16) What has been your most frustrating bug or error in this class so far? How did you troubleshoot or fix it?
+# When I was try to git push origin main for the first time, I kept getting the message that my permission was denied and that the repo might not exist. I was eventually able to overcome it by searching up my error message online and following other troubleshooting methods people with the same issue in the past tried, and by removing the repository and recreating it, I was somehow able to get it to work.
+
+# 17) What’s a question you still have? What’s something you’re confused about?
+# I'm still kind of confused about the difference between saving to a local repository and a remote repository.
+
+# 18) Tell me a fun fact!
+# Bears use rocks to scratch themselves.
+
+# 19) Print your favorite math expression you've learned in Python so far. 
+# (Hint: Use print() and add a comment explaining what it does.)
+print(5 % 3) # modulo gives you the remainder
